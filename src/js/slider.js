93be797ead32d2df2909f44f2a05356158e86119
Eduard <sorokolietov@gmail.com>
2023-06-18 13:57:47 +0200

    add all

---
+++ src/js/slider.js
@@ -0,0 +1,37 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+    loop: false,
+    slidesPerView: 1,
+    breakpoints: {
+        375: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 16,
+        },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 28,
+        },
+  },
+
+  // If we need pagination
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+  },
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+  
+});
