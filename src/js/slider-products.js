1605199571dd30b396d25466c9a281ef3ca290a4
Denis <grabko.denis11@gmail.com>
2023-06-20 21:42:49 +0300

    our-products add finally-slider final-work all

--- src/js/slider-products.js
+++ src/js/slider-products.js
@@ -1,22 +1,17 @@
-
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 1,      
+var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
         loop: true,
         speed: 1000,
       pagination: {
-        el: ".swiper-pagination",
+        el: ".swiper-pagination-p",
         clickable: true,
         },
-      
         breakpoints: {
             768: {
                 slidesPerView: 3,
             },
-
             1200: {
                 slidesPerView: 4,
             },
-          
         },
     });
-  
